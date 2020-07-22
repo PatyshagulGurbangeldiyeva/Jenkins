cda57a2bbd4ea65406196810a78fc9095d42ae3b
Vepa Gurbangeldiyev <vepagurbangeldiyev@vepas-mbp.home>
2020-07-22 18:33:23 -0500

    changes checking

---
+++ src/test/java/com/hrms/practice/JenkisPracticeChanges.java
@@ -0,0 +1,11 @@
+package com.hrms.practice;
+
+public class JenkisPracticeChanges {
+
+	public static void main(String[] args) {
+		
+		System.out.println("here I execute in order to see my changes in Jenkins");
+
+	}
+
+}
